bd0cab492d209ef84ae697593d7cd16ca01b6304
sambot <chatbotrops@gmail.com>
2017-07-20 13:42:29 +0200

    ttoto3

--- calendarbusiness.js
+++ calendarbusiness.js
@@ -1,8 +1,66 @@
 'use strict';
-var CLIENTGOOGLE=require('./clientgoogle');
-
-var jwtClient=CLIENTGOOGLE.jwtClient;
+var google = require('googleapis');
+var AWSCLIENT = require('./clientaws');
 // fonctions test
+var id_calendrier=process.env.CALENDAR_ID;
+
+/// CREATION DU JWT CLIENT 
+
+
+// get json secret google from s3
+var KEY_NAME="secret-code.json";
+var BUCKET_NAME=process.env.BUCKET_NAME;
+ var params = {Bucket: BUCKET_NAME, Key: KEY_NAME};
+var jwtClient;
+
+
+AWSCLIENT.s3.getObject(params, function(err, json_data)
+   {
+      if (!err) {
+        var jsonsecret = JSON.parse(new Buffer(json_data.Body).toString("utf8"));
+        var client_email=jsonsecret.client_email;
+var private_key=jsonsecret.private_key;//process.env.GOOGLE_PRIVATE_KEY; // defined in Heroku
+console.log(client_email);
+
+jwtClient = new google.auth.JWT(
+  client_email,
+  null,
+  private_key,
+  ['https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/calendar'], // an array of auth scopes
+  null
+);
+if(jwtClient==null)
+{
+console.log('pb google client');
+}
+else
+{
+console.log('google client ok');
+}
+
+//console.log(json);
+     //jsonsecret=json;
+     // return json;
+       // PROCESS JSON DATA
+     //      ......
+    }
+else
+{
+ console.log("Error getting json from S3", err);
+
+}
+   });
+
+
+
+
+
+
+
+
+/// FIN CREATION DU JWT CLIENT
+
+
 
 
 exports.getevents=function()
