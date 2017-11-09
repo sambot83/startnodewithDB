79ca90f0bb99eb36d18ca9759919bae31a1c49a5
sambot <chatbotrops@gmail.com>
2017-11-09 10:21:06 +0100

    f commit

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
 
 
-const restService = express();
-restService.use(bodyParser.json());
 
-restService.post('/hook', function (req, res) {
-
-    console.log('hook request');
-   
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
-    
-   
-   
-    
-
-
-  
-});
+console.log('hello');
 
 restService.listen((process.env.PORT || 5000), function () {
     console.log("Server listening");
